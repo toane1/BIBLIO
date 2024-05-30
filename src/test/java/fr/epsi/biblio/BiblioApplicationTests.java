56e715db2b173f122a985507a43d79fb46db3523
antoine <archer.antoine@gmail.com>
2024-05-30 23:03:26 +0200

    init

---
+++ src/test/java/fr/epsi/biblio/BiblioApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.biblio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
